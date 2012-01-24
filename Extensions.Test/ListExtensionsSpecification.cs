9bf2d0b06b85d95aa2faf1b5f4f58e70bb74407f
Todd Meinershagen <todd@meinershagen.net>
2012-01-23 20:53:49 -0600

    initial commit

---
+++ Extensions.Test/ListExtensionsSpecification.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace Extensions.Test
+{
+    [TestFixture]
+    public class when_requesting_siblings_after_an_index
+    {
+        [Test]
+        public void given_null_list_should_throw_argument_null_exception()
+        {
+            List<string> items = null;
+            var index = 0;
+            Action act = () => items.SiblingsAfter(index);
+            act.ShouldThrow<ArgumentNullException>().WithMessage("Value cannot be null.\r\nParameter name: list");
+        }
+
+        [Test]
+        public void given_empty_list_should_throw_argument_exception()
+        {
+            var items = new List<string>();
+            var index = 0;
+            Action act = () => items.SiblingsAfter(index);
+            act.ShouldThrow<IndexOutOfRangeException>();
+        }
+
+        [Test]
+        public void given_list_with_less_items_than_current_index_should_throw_argument_null_exception()
+        {
+            var items = new List<string> {"a", "b", "c"};
+            var index = 3;
+            Action act = () => items.SiblingsAfter(index);
+            act.ShouldThrow<IndexOutOfRangeException>();
+        }
+
+        [Test]
+        public void given_list_with_items_and_current_index_is_equal_to_last_item_should_return_empty_list()
+        {
+            var items = new List<string> {"a", "b"};
+            var index = 1;
+            var remaining = items.SiblingsAfter(index);
+            remaining.Should().BeEmpty();
+        }
+
+        [Test]
+        public void given_list_with_items_should_return_items_after_the_index_given()
+        {
+            var items = new List<string> {"a", "b", "c", "d", "e"};
+            var index = 2;
+            var remaining = items.SiblingsAfter(index);
+            remaining.Should().HaveCount(2);
+            remaining.Should().Contain("d");
+            remaining.Should().Contain("e");
+        }
+    }
+}
---
+++ Extensions/ListExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Extensions
+{
+    public static class ListExtensions
+    {
+        public static List<T> SiblingsAfter<T>(this List<T> list, int currentIndex)
+        {
+            if (list == null)
+                throw new ArgumentNullException("list");
+
+            if (currentIndex > list.Count - 1 || currentIndex < 0)
+                throw new IndexOutOfRangeException();
+
+            return currentIndex == list.Count - 1 ? new List<T>() : list.GetRange(currentIndex + 1, list.Count - currentIndex - 1);
+        }
+
+    }
+}
