c171cb333433200e4ba9be5c083cb244df589d6a
Todd Meinershagen <todd@meinershagen.net>
2012-03-31 14:03:08 -0500

    added string[].Concat(startIndex, endIndex, separator) to allow selective concatentation of string elements.

---
+++ Extensions.Test/ArrayOfStringExtensionsSpecification.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace Extensions.Test
+{
+    [TestFixture]
+    public class ArrayOfStringExtensionsSpecification
+    {
+        private string _separator = ",";
+        private string _testValue1 = "testValue1";
+        private string _testValue2 = "testValue2";
+        private string _testValue3 = "testValue3";
+        private string _testValue4 = "testValue4";
+        private string _testValue5 = "testValue5";
+        private string[] _values;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _values = new[] { _testValue1, _testValue2, _testValue3, _testValue4, _testValue5 };
+        }
+
+        [Test]
+        public void given_start_and_end_index_should_return_concatenation_of_elements_that_fall_within_the_range()
+        {       
+            _values.Concat(1, 2).Should().Be(string.Concat(_testValue2, _testValue3));
+        }
+
+        [Test]
+        public void given_start_and_end_index_and_separator_should_return_concatenation_of_elements_that_fall_within_the_range_with_separators()
+        {
+            _values.Concat(2, 3, _separator).Should().Be(string.Concat(_testValue3, _separator, _testValue4));
+        }
+
+        [Test]
+        public void given_start_index_should_return_concatenation_of_elements_between_start_index_and_end_of_array()
+        {
+            _values.Concat(2).Should().Be(string.Concat(_testValue3, _testValue4, _testValue5));
+        }
+
+        [Test]
+        public void given_start_index_and_separator_should_return_concatenation_of_elements_between_start_index_and_end_of_array_with_separators()
+        {
+            _values.Concat(3, _separator).Should().Be(string.Concat(_testValue4, _separator, _testValue5));
+        }
+
+        [Test]
+        public void given_no_parameters_should_return_concatenation_of_all_elements_in_array()
+        {
+            _values.Concat().Should().Be(string.Concat(_testValue1, _testValue2, _testValue3, _testValue4, _testValue5));
+        }
+
+        [Test]
+        public void given_separator_should_return_concatenation_of_all_elements_in_array_with_separators()
+        {
+            _values.Concat(_separator).Should().Be(string.Concat(_testValue1, _separator, _testValue2, _separator, _testValue3, _separator, _testValue4, _separator, _testValue5));
+        }
+
+        [Test]
+        public void given_start_index_less_than_existing_indexes_should_throw_index_out_of_range_exception()
+        {
+            Action action = () => _values.Concat(-1);
+            action.ShouldThrow<IndexOutOfRangeException>().WithMessage("'startIndex' was out of the range of acceptable values for this list.");
+        }
+
+        [Test]
+        public void given_end_index_greater_than_existing_indexes_should_throw_index_out_of_range_exception()
+        {
+            Action action = () => _values.Concat(1, 6);
+            action.ShouldThrow<IndexOutOfRangeException>().WithMessage("'endIndex' was out of the range of acceptable values for this list.");
+        }
+
+        [Test]
+        public void given_start_index_greater_than_end_index_should_throw_index_out_of_range_exception()
+        {
+            Action action = () => _values.Concat(4, 1);
+            action.ShouldThrow<IndexOutOfRangeException>().WithMessage("'startIndex' cannot be greater than 'endIndex'.");
+        }
+    }
+}
---
+++ Extensions/ArrayOfStringsExtensions.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+
+namespace Extensions
+{
+    public static class ArrayOfStringsExtensions
+    {
+        public static string Concat(this string[] values, string separator = "")
+        {
+            return values.Concat(0, separator);
+        }
+
+        public static string Concat(this string[] values, int startIndex, string separator = "")
+        {
+            return values.Concat(startIndex, values.Length - 1, separator);
+        }
+
+        public static string Concat(this string[] values, int startIndex, int endIndex, string separator = "")
+        {
+            var message = "'{0}' was out of the range of acceptable values for this list.";
+
+            if (startIndex > endIndex)
+            {
+                throw new IndexOutOfRangeException("'startIndex' cannot be greater than 'endIndex'.");    
+            }
+
+            if (startIndex < 0)
+            {
+                throw new IndexOutOfRangeException(string.Format(message, "startIndex"));
+            }
+
+            if (endIndex + 1 > values.Length)
+            {
+                throw new IndexOutOfRangeException(string.Format(message, "endIndex"));
+            }
+
+            return string.Join(separator, values.ToList().Skip(startIndex).Take(endIndex - startIndex + 1));
+        }
+    }
+}
